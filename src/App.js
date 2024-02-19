d8820ee27503a66f735d0970083dabe0a784d1ee
pawel <kozyra.mail@gmail.com>
2024-02-19 13:14:22 +0100

    UseStare and useEffect example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Surname: {props.surname}</h2>
-      <p>Age: {props.age}</p>
-      <br />
-    </>
-  );
-}
 
 const App = () => {
-  const name = "world!"
-  const bool = true
+
+  const [counter, setCounter] = useState(0)
+  
+  useEffect(() => {
+    alert(`Counter: ${counter}`)
+  }, [counter])
+
   return (
     <div className="App">
-      <Person name='John' surname='Doe' age={30} />
-      <Person name='Jane' surname='C' age={43} />
-      <Person name='X' surname='Y' age={12} />
+      <button onClick = {modifyValueByChange(-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick = {modifyValueByChange(1)}>+</button>
+
     </div>
   );
+
+  function modifyValueByChange(change) {
+    return () => (setCounter(counter + change));
+  }
 }
 
 export default App;
